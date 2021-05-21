01efc62462c9e73106dc227ded3872cebfe52f6a
Nika Perfilova <ees2009.2014@gmail.com>
2021-05-21 13:03:08 +0700

    Добавлена ошибка для проверки линтера

--- src/cli.js
+++ src/cli.js
@@ -3,6 +3,6 @@ import { question } from 'readline-sync';
 const greet = () => {
   const name = question('May I have your name? ');
   console.log(`Hello, ${name}!`);
-};
+}
 
 export default greet;
