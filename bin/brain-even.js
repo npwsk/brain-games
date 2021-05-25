63510e342a74fecca5bd37d304a7212968e937e7
Nika Perfilova <ees2009.2014@gmail.com>
2021-05-25 18:23:24 +0700

    Добавлен процесс игры

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,1 +1,17 @@
 #!/usr/bin/env node
+import * as cli from '../src/cli.js';
+import { generateNumber, checkAnswer, getCorrectAnswer } from '../src/core.js';
+
+const userName = cli.getUserName();
+for (let round = 1; round <= 3; round += 1) {
+  const number = generateNumber();
+  const userAnswer = cli.getUserAnswer(number);
+  const correctAnswer = getCorrectAnswer(number);
+  if (!checkAnswer(number, userAnswer)) {
+    cli.printDebugMessage(userAnswer, correctAnswer);
+    cli.printLossMessage(userName);
+    process.exit();
+  }
+  cli.printSuccessMessage();
+}
+cli.printWinMessage(userName);
