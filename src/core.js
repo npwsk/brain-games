7b17a9df7c0e1c203fd6e7e8ef0ae211271efdb0
Nika Perfilova <ees2009.2014@gmail.com>
2021-05-25 17:40:37 +0700

    Добавлены функции для вычисления верного ответа и проверки ответа

--- src/core.js
+++ src/core.js
@@ -3,4 +3,18 @@ const maxGeneratedNumber = 100;
 
 const generateNumber = () => Math.floor(Math.random() * maxGeneratedNumber);
 
-export default generateNumber;
+const isEven = (number) => number % 2 === 0;
+
+const getCorrectAnswer = (number) => (isEven(number) ? 'yes' : 'no');
+
+const checkAnswer = (number, userAnswer) => {
+  const correctAnswer = getCorrectAnswer(number);
+  if (userAnswer === correctAnswer) {
+    return true;
+  }
+  return false;
+};
+
+export {
+  generateNumber, checkAnswer,
+};
