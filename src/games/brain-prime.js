a5d1e425bc439c9dd9d9a605cba0467747593432
Nika Perfilova <ees2009.2014@gmail.com>
2021-05-27 12:55:22 +0700

    Добавлена реализация логики игры brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,5 +1,24 @@
 import { startGame as startAnyGame, generateNumber } from '../index.js';
 
-const startGame = () => startAnyGame();
+const maxRandomNumber = 100;
+const instructionsMessage = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const getRandomNumber = () => generateNumber(maxRandomNumber);
+
+const isPrime = (number) => {
+  if (number === 1) {
+    return false;
+  }
+  for (let possibleDivisor = 2; possibleDivisor <= Math.sqrt(number); possibleDivisor += 1) {
+    if (number % possibleDivisor === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const getCorrectAnswer = (number) => (isPrime(number) ? 'yes' : 'no');
+
+const startGame = () => startAnyGame(instructionsMessage, getRandomNumber, getCorrectAnswer);
 
 export default startGame;
